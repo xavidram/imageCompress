6e0b08165ae74e5ac3c002c37adba6ef5f54bad0
Xavid <xavid.ramirez01>
2016-02-26 23:20:28 -0600

    fixed typo

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
 	plugins = require('gulp-load-plugins')();
 
 gulp.task('compress' , function(){
-	return gulps.src('./uncompressed')
+	return gulp.src('./uncompressed')
 		.pipe(plugins.imagemin({
 			progressive : true,
 			svgoPlugins : [
